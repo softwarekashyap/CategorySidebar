ac01b25fb8dd691fb9eb6be258a00486fac8c930
RKs <softwarekashyap@gmail.com>
2020-03-02 23:40:07 +0530

    Second Update

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Kashyap_Sidebar',
+    'Kashyap_CategorySidebar',
     __DIR__
 );
